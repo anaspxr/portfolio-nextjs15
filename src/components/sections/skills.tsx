6e0dd6c3cdcc09863be5b3897dde73ed049cb879
ANAS <anaspappadan@gmail.com>
2025-01-20 17:22:07 +0530

    add TechCard component and update skills section with descriptions and links

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -1,30 +1,17 @@
 import { skills } from "@/lib/skills";
-import Image from "next/image";
+import TechCard from "../ui/tech-card";
 
 export default function Skills() {
   return (
     <section
       id="skills"
-      className="mx-2 sm:mx-8 mb-8 md:mx-16 lg:mx-32 p-4  bg-black bg-opacity-50 border rounded-md border-opacity-50 border-purple-500  ">
-      <h2 className="text-3xl mb-2 text-center">My skills</h2>
+      className="relative mx-2 sm:mx-8 mb-8 md:mx-16 lg:mx-32 p-4  bg-black bg-opacity-50 border rounded-md border-opacity-50 border-purple-500 ">
+      <h2 className="text-3xl mb-2 text-center">Technologies I use</h2>
       <div
         className={`mb-8 h-1 w-32 m-auto bg-violet-800 transition-[width] duration-700 ease-in-out`}></div>
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4  m-auto justify-center">
         {skills.map((skill, index) => (
-          <div
-            key={index}
-            className=" p-4 flex items-center justify-center hover:scale-105 transition-transform  bg-violet-500 bg-opacity-20 rounded-md">
-            <div className="flex flex-col items-center">
-              <Image
-                height={512}
-                width={512}
-                src={skill.icon}
-                alt={skill.name}
-                className="h-16 w-16 object-contain"
-              />
-              <p className="text-center text-white">{skill.name}</p>
-            </div>
-          </div>
+          <TechCard key={index} {...skill} />
         ))}
       </div>
     </section>
---
+++ src/components/ui/tech-card.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function TechCard({
+  name,
+  icon,
+  description,
+  link,
+}: {
+  name: string;
+  icon: string;
+  description: string;
+  link?: string;
+}) {
+  return (
+    <div className="group relative p-4 flex items-center justify-center hover:scale-105 transition-transform duration-300  bg-violet-500 bg-opacity-20 rounded-md overflow-hidden">
+      <div className="flex flex-col items-center group-hover:blur-[1px] transition-opacity duration-300">
+        <Image
+          height={512}
+          width={512}
+          src={icon}
+          alt={name}
+          className="h-16 w-16 object-contain"
+        />
+        <p className="text-center text-white">{name}</p>
+      </div>
+      <div className="w-full h-full gap-y-2 flex flex-col justify-center  p-2 absolute  left-0 text-xs translate-y-full group-hover:translate-y-0 transition-transform bg-black bg-opacity-50 duration-300">
+        {description}
+        {link && (
+          <Link
+            href={link}
+            className="text-violet-500 hover:text-violet-400 hover:underline">
+            Learn more
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}
